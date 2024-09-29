ea1c8c8612f0e065ea52e2146c2254a4c4e2a5fc
Ven-om-codes <kumbhar.om.it@kgce.edu.in>
2024-09-29 14:17:50 +0000

    I've UPDATED AdvDevopsTwo.py

--- AdvDevOpsSecondEXP.py
+++ AdvDevOpsSecondEXP.py
@@ -1,1 +1,4 @@
 print("Hello,WELCOME TO AWS CLOUD9 SERVICES")
+print("<<<<<<<<<<WELCOME TO AWS CLOUD9>>>>>>>>>>")
+print("I am OM & I've succesfully connected this project to my GitHub Account")
+print("<<<<<<<<<<THANK YOU>>>>>>>>>>")
